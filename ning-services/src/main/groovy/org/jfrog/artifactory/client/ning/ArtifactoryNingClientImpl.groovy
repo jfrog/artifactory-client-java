8b3f882a011f28f14b4e8685ff3c6708ff6e8e69
freddy33 <freds@jfrog.com>
2014-03-07 17:46:46 -0800

    Provide a default logback.xml file

--- ning-services/src/main/groovy/org/jfrog/artifactory/client/ning/ArtifactoryNingClientImpl.groovy
+++ ning-services/src/main/groovy/org/jfrog/artifactory/client/ning/ArtifactoryNingClientImpl.groovy
@@ -6,26 +6,23 @@
  * limitations under the License. See accompanying LICENSE file.  */
 package org.jfrog.artifactory.client.ning
 
-import static groovyx.net.http.ContentType.*
-import static groovyx.net.http.Method.*
-
-import java.util.Collection;
-import java.util.Map;
-
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.ning.http.client.AsyncHttpClient
+import com.ning.http.client.AsyncHttpClient.BoundRequestBuilder
+import com.ning.http.client.Request
+import com.ning.http.client.Response
 import groovyx.net.http.ContentType
 import groovyx.net.http.Method
 import groovyx.net.http.RESTClient
 import net.sf.json.JSON
-
+import org.apache.http.client.HttpResponseException
+import org.jfrog.artifactory.client.impl.ArtifactoryImpl
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.ning.http.client.AsyncHttpClient
-import com.ning.http.client.Cookie
-import com.ning.http.client.Request
-import com.ning.http.client.Response
-import com.ning.http.client.AsyncHttpClient.BoundRequestBuilder
+import static groovyx.net.http.ContentType.ANY
+import static groovyx.net.http.ContentType.TEXT
+import static groovyx.net.http.Method.*
 
 /**
  * This class simply extends {@link ArtifactoryImpl} to keep existing API
@@ -35,14 +32,16 @@ import com.ning.http.client.AsyncHttpClient.BoundRequestBuilder
  * @author charlesk
  *
  */
-public class ArtifactoryNingClientImpl extends org.jfrog.artifactory.client.impl.ArtifactoryImpl {
+public class ArtifactoryNingClientImpl extends ArtifactoryImpl {
     private static final Logger log = LoggerFactory.getLogger(ArtifactoryNingClientImpl.class);
     private static final ObjectMapper objectMapper = new ObjectMapper();
     private final AsyncHttpClient ningHttpClient;
     private final String url;
     private final NingRequest ningRequest;
 
-    public ArtifactoryNingClientImpl(final AsyncHttpClient ningHttpClient, final String context, final String host, final NingRequest ningRequest){
+    public ArtifactoryNingClientImpl(
+            final AsyncHttpClient ningHttpClient,
+            final String context, final String host, final NingRequest ningRequest) {
         //Setup a mock client but won't be used.
         super(new RESTClient(), null);
         this.ningRequest = ningRequest;
@@ -54,19 +53,23 @@ public class ArtifactoryNingClientImpl extends org.jfrog.artifactory.client.impl
         get(path, [:], responseContentType, responseClass, headers)
     }
 
-    def <T> T get(String path, Map query, ContentType responseContentType = ANY, def responseClass = null, Map headers = null) {
+    def <T> T get(String path, Map query, ContentType responseContentType = ANY,
+                  def responseClass = null, Map headers = null) {
         rest(GET, path, query, responseContentType, responseClass, ANY, null, headers)
     }
 
-    def <T> T delete(String path, Map query = null, ContentType responseContentType = ANY, def responseClass = null, Map addlHeaders = null) {
+    def <T> T delete(String path, Map query = null, ContentType responseContentType = ANY,
+                     def responseClass = null, Map addlHeaders = null) {
         rest(DELETE, path, query, responseContentType, responseClass, TEXT, null, addlHeaders)
     }
 
-    def <T> T post(String path, Map query = null, ContentType responseContentType = ANY, def responseClass = null, ContentType requestContentType = JSON, requestBody = null, Map addlHeaders = null, long contentLength = -1) {
+    def <T> T post(String path, Map query = null, ContentType responseContentType = ANY,
+                   def responseClass = null, ContentType requestContentType = JSON, requestBody = null, Map addlHeaders = null, long contentLength = -1) {
         rest(POST, path, query, responseContentType, responseClass, requestContentType, requestBody, addlHeaders, contentLength)
     }
 
-    def <T> T put(String path, Map query = null, ContentType responseContentType = ANY, def responseClass = null, ContentType requestContentType = JSON, requestBody = null, Map addlHeaders = null, long contentLength = -1) {
+    def <T> T put(String path, Map query = null, ContentType responseContentType = ANY,
+                  def responseClass = null, ContentType requestContentType = JSON, requestBody = null, Map addlHeaders = null, long contentLength = -1) {
         rest(PUT, path, query, responseContentType, responseClass, requestContentType, requestBody, addlHeaders, contentLength)
     }
 
@@ -83,12 +86,13 @@ public class ArtifactoryNingClientImpl extends org.jfrog.artifactory.client.impl
         return handleResponse(ningHttpClient.executeRequest(request).get(), finalUrl, false);
     }
 
-    private def <T> T rest(Method method, String path, Map query = null, responseType = ANY, def responseClass, ContentType requestContentType = JSON, requestBody = null, Map addlHeaders = null, long contentLength = -1 ) {
+    private def <T> T rest(Method method, String path, Map query = null, responseType = ANY,
+                           def responseClass, ContentType requestContentType = JSON, requestBody = null, Map addlHeaders = null, long contentLength = -1) {
         log.debug("Method: {}, Path: {}", method, path);
         String finalUrl = this.url + path;
         log.debug("finalUrl: {}", finalUrl);
         BoundRequestBuilder requestBuilder;
-        switch (method){
+        switch (method) {
             case POST:
                 requestBuilder = ningHttpClient.preparePost(finalUrl).setBody(requestBody);
                 break;
@@ -114,46 +118,46 @@ public class ArtifactoryNingClientImpl extends org.jfrog.artifactory.client.impl
         return null;
     }
 
-    private Request customizeRequest(BoundRequestBuilder boundRequestBuilder, Map addlHeaders, Map query){
+    private Request customizeRequest(BoundRequestBuilder boundRequestBuilder, Map addlHeaders, Map query) {
         BoundRequestBuilder customRequestBuilder = this.ningRequest.getBoundRequestBuilder(boundRequestBuilder);
-        if (customRequestBuilder==null){
+        if (customRequestBuilder == null) {
             customRequestBuilder = boundRequestBuilder;
         }
-        if (addlHeaders !=null){
-            for (e in addlHeaders){
+        if (addlHeaders != null) {
+            for (e in addlHeaders) {
                 customRequestBuilder.setHeader(e.key, (String) e.value);
             }
         }
-        if (query !=null){
-            for (e in query){
+        if (query != null) {
+            for (e in query) {
                 customRequestBuilder.addQueryParameter(e.key, (String) e.value);
             }
         }
         return customRequestBuilder.build();
     }
 
-    private InputStream handleResponse(Response response, String url, boolean isBinary) throws IOException{
+    private InputStream handleResponse(Response response, String url, boolean isBinary) throws IOException {
         int statusCode = response.getStatusCode();
         log.debug("Artifact path: " + url + " Response status code: " + response.getStatusCode()
                 + " status text: " + response.getStatusText() + " body: "
                 + ((isBinary) ? " binary " : response.getResponseBody()));
         switch (statusCode) {
-            // 200 series are ok.
+        // 200 series are ok.
             case 200:
             case 201:
             case 202:
                 return response.getResponseBodyAsStream();
             case 404:
-            // not found 404
+                // not found 404
             case 401:
-            // auth failures 401/403
+                // auth failures 401/403
             case 403:
-            //Throw apache http client's HttpResponseException since the API expects that.
-                throw new org.apache.http.client.HttpResponseException(statusCode, response.getStatusText());
+                //Throw apache http client's HttpResponseException since the API expects that.
+                throw new HttpResponseException(statusCode, response.getStatusText());
             default:
                 //For all other types, throw IOException.
                 throw new IOException("Response was " + statusCode
-                + " message '" + response.getStatusText() + "'");
+                        + " message '" + response.getStatusText() + "'");
         }
     }
 }
