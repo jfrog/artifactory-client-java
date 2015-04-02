8f874a39fdea19bc70f4c7b965cb595d37fe8bcd
Igor Sakovich <ragesteel@gmail.com>
2015-04-02 18:13:07 +0300

    #52 Adding GAVC and artifact version search.

--- api/src/main/java/org/jfrog/artifactory/client/Searches.java
+++ api/src/main/java/org/jfrog/artifactory/client/Searches.java
@@ -13,11 +13,23 @@ public interface Searches {
 
     Searches repositories(String... repositories);
 
+    Searches groupId(String groupId);
+
+    Searches artifactId(String artifactId);
+
+    Searches version(String version);
+
+    Searches classifier(String classifier);
+
     Searches artifactsByName(String name);
 
     Searches artifactsCreatedSince(long sinceMillis);
 
     Searches artifactsCreatedInDateRange(long fromMillis, long toMillis);
+
+    Searches artifactVersion();
+
+    Searches artifactGAVC();
 
     List<RepoPath> doSearch();
 
