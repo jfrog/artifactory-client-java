842bc74a3c29d064d0ca21e7996c922b8544e7ea
Ihor Banadiga <banadiga@gmail.com>
2017-06-14 17:59:15 +0300

    rewrite in java style

--- services/src/main/groovy/org/jfrog/artifactory/client/impl/RepositoryHandleImpl.groovy
+++ services/src/main/groovy/org/jfrog/artifactory/client/impl/RepositoryHandleImpl.groovy
@@ -67,9 +67,9 @@ class RepositoryHandleImpl implements RepositoryHandle {
         def settings = artifactory.parseText(json, RepositorySettings)
         XraySettingsImpl xray = artifactory.parseText(json, XraySettingsImpl)
 
-        repo.setRepositorySettings settings
-        repo.setXraySettings xray
-        repo.otherProperties = getOtherProperties json, repo
+        repo.setRepositorySettings(settings)
+        repo.setXraySettings(xray)
+        repo.otherProperties = getOtherProperties(json, repo)
 
         repo
     }
@@ -78,11 +78,11 @@ class RepositoryHandleImpl implements RepositoryHandle {
         Map otherProperties = artifactory.parseText json, Map
 
         def knownKeys = [] as Set
-        knownKeys.addAll extractProperties(repo)
-        knownKeys.addAll extractProperties(repo.xraySettings)
-        knownKeys.addAll extractProperties(repo.repositorySettings)
+        knownKeys.addAll(extractProperties(repo))
+        knownKeys.addAll(extractProperties(repo.xraySettings))
+        knownKeys.addAll(extractProperties(repo.repositorySettings))
 
-        otherProperties.keySet().removeAll knownKeys
+        otherProperties.keySet().removeAll(knownKeys)
         otherProperties
     }
 
