f234e27a095d2c27c562ae6a8fc6b689d82edde5
Dávid Csákvári <dodiehun@gmail.com>
2019-10-16 08:20:13 +0200

    Fix 206: Improve error handling (#225)
    * Fix #206: Set message of CopyMoveException
    
    * Fix #206: Preserve original content on parse error

--- services/src/main/groovy/org/jfrog/artifactory/client/impl/CopyMoveException.groovy
+++ services/src/main/groovy/org/jfrog/artifactory/client/impl/CopyMoveException.groovy
@@ -2,17 +2,26 @@ package org.jfrog.artifactory.client.impl
 
 import org.jfrog.artifactory.client.model.CopyMoveResultReport
 
+import java.util.stream.Collectors
+
 /**
  * Created by Jeka on 05/11/13.
  */
-class CopyMoveException extends RuntimeException {
+public class CopyMoveException extends RuntimeException {
     private CopyMoveResultReport copyMoveResultReport
 
     CopyMoveException(CopyMoveResultReport copyMoveResultReport) {
+        super(concatMessages(copyMoveResultReport))
         this.copyMoveResultReport = copyMoveResultReport
     }
 
     CopyMoveResultReport getCopyMoveResultReport() {
         return copyMoveResultReport
+    }
+
+    private static String concatMessages(CopyMoveResultReport copyMoveResultReport) {
+        copyMoveResultReport.getMessages().stream()
+                .map { result -> result.getMessage() }
+                .collect(Collectors.joining(", "))
     }
 }
