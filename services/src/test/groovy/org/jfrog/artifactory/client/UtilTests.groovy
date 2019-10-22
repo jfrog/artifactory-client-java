b023f3879891b3dc4d7682a509e82a1c530f6281
Dávid Csákvári <dodiehun@gmail.com>
2019-10-22 12:02:04 +0200

    Get RepositorySettingsImpl for a PackageType #267

---
+++ services/src/test/groovy/org/jfrog/artifactory/client/UtilTests.groovy
@@ -0,0 +1,19 @@
+package org.jfrog.artifactory.client
+
+import org.hamcrest.CoreMatchers
+import org.jfrog.artifactory.client.impl.util.Util
+import org.jfrog.artifactory.client.model.impl.PackageTypeImpl
+import org.jfrog.artifactory.client.model.repository.settings.impl.BowerRepositorySettingsImpl
+import org.jfrog.artifactory.client.model.repository.settings.impl.MavenRepositorySettingsImpl
+import org.junit.Assert
+import org.testng.annotations.Test
+
+public class UtilTests {
+
+    @Test(groups = "utilTests")
+    public void getRepositorySettingsForTypeTest() {
+        Assert.assertThat(Util.getRepositorySettingsClassForPackageType(PackageTypeImpl.bower), CoreMatchers.equalTo(BowerRepositorySettingsImpl.class))
+        Assert.assertThat(Util.getRepositorySettingsClassForPackageType(PackageTypeImpl.maven), CoreMatchers.equalTo(MavenRepositorySettingsImpl.class))
+    }
+
+}
