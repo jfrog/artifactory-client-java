ca2ce00c694e1247e795536ff61282c10706187f
Asaf Gabai <asafg@jfrog.com>
2021-06-10 14:58:59 +0300

    Removed AQL service

--- services/src/main/java/org/jfrog/artifactory/client/aql/AqlBuilderException.java
+++
@@ -1,7 +0,0 @@
-package org.jfrog.artifactory.client.aql;
-
-public class AqlBuilderException extends RuntimeException {
-    public AqlBuilderException(String message, Throwable cause) {
-    super(message, cause);
-  }
-}
