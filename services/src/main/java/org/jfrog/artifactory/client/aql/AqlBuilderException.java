064529aa30eb084fdf735a8bacdd10e07f15e9cc
Asaf Gabai <asafg@jfrog.com>
2021-06-16 09:38:59 +0300

    AQL service is back (some users use it)

---
+++ services/src/main/java/org/jfrog/artifactory/client/aql/AqlBuilderException.java
@@ -0,0 +1,7 @@
+package org.jfrog.artifactory.client.aql;
+
+public class AqlBuilderException extends RuntimeException {
+    public AqlBuilderException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
