9dc79c658224f6fa116e6f17d765882808e628b2
yahavi <yahavi@jfrog.com>
2021-07-20 09:27:43 +0300

    Docker tests should create remote repo with cache

--- services/src/test/groovy/org/jfrog/artifactory/client/DockerPackageTypeRepositoryTests.groovy
+++ services/src/test/groovy/org/jfrog/artifactory/client/DockerPackageTypeRepositoryTests.groovy
@@ -33,6 +33,7 @@ public class DockerPackageTypeRepositoryTests extends BaseRepositoryTests {
 
     @BeforeMethod
     protected void setUp() {
+        storeArtifactsLocallyInRemoteRepo = true
         super.setUp()
     }
 
