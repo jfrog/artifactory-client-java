1990f2866e3ec5041f145f86801301f718c56f2b
omers <omers@jfrog.com>
2022-03-29 11:25:23 +0300

    Add HTTP processor to client.

--- httpClient/src/main/java/org/jfrog/artifactory/client/httpClient/http/HttpBuilderBase.java
+++ httpClient/src/main/java/org/jfrog/artifactory/client/httpClient/http/HttpBuilderBase.java
@@ -194,6 +194,16 @@ public abstract class HttpBuilderBase<T extends HttpBuilderBase<?>> {
         return self();
     }
 
+    public T addInterceptorFirst(HttpRequestInterceptor interceptor) {
+        this.builder.addInterceptorFirst(interceptor);
+        return this.self();
+    }
+
+    public T addInterceptorLast(HttpResponseInterceptor interceptor) {
+        this.builder.addInterceptorLast(interceptor);
+        return this.self();
+    }
+
     public HttpBuilderBase<?>.ProxyConfigBuilder proxy(String host, int port) {
         return new HttpBuilderBase<?>.ProxyConfigBuilder(host, port);
     }
